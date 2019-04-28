e17f1454c0ecba62a771412c61ef8c7fb0fd8c41
songhm <songhm@qssec.com>
2019-04-28 11:10:59 +0800

    [通用] 增加数据库配置

--- django_example/settings/product.py
+++ django_example/settings/product.py
@@ -2,3 +2,18 @@
 
 from .base import *
 
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_example',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'CONN_MAX_AGE': 5 * 60,
+        'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
