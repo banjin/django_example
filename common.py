71d24ca936b498d3155e18ee68a29cef59e8f6d2
songhm <songhm@qssec.com>
2019-04-28 12:48:21 +0800

    手动实现缓存

--- common.py
+++ common.py
@@ -1,8 +1,9 @@
 # coding:utf-8
 
 import time
-
+from collections import OrderedDict
 from functools import  wraps
+# from functools import lru_cache 已经是标准库中的一部分
 
 def time_it(func):
     """
@@ -10,7 +11,7 @@ def time_it(func):
     :param func:
     :return:
     """
-    @wraps
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
@@ -18,3 +19,82 @@ def time_it(func):
         return result
 
     return wrapper
+
+
+class LRUCacheDict:
+    def __init__(self, max_size=1024, expiration=60):
+        """
+        最大容量是1024个key,每个key的有效期为60s
+        :param max_size:
+        :param expiration:
+        """
+        self.max_size = max_size
+        self.expiration = expiration
+        self._cache = {}
+        self._access_records = OrderedDict() # 记录访问时间
+        self._expire_records = OrderedDict() # 记录生效时间
+
+
+    def __setitem__(self, key, value):
+        now = int(time.time())
+        self.__delete__(key)
+
+        self._cache[key] = value
+        self._expire_records[key] = now + self.expiration
+        self._access_records[key] = now
+
+        self.cleanup()
+
+    def __getitem__(self, key):
+        now = int(time.time())
+        del self._access_records[key]
+        self._access_records[key] = now
+        self.cleanup()
+        return self._cache[key]
+
+    def __contains__(self, key):
+        self.cleanup()
+        return key in self._cache
+
+    def __delete__(self, key):
+        if key in self._cache:
+            del self._cache[key]
+            del self._access_records[key]
+            del self._expire_records[key]
+
+    def cleanup(self):
+        if self.expiration is None:
+            return None
+        pending_delete_keys = []
+        now = int(time.time())
+        # 删除 已经过期的缓存
+        for k, v in self._expire_records.items():
+            if v < now:
+                pending_delete_keys.append(k)
+        for del_k in pending_delete_keys:
+            self.__delete__(del_k)
+        # 如果数据量大于 max_size， 则删掉最旧的缓存
+        while len(self._cache) > self.max_size:
+            for k in self._access_records:
+                self.__delete__(k)
+                break
+
+
+
+def cache_it(max_size=1024, expiration=60):
+    CACHE = LRUCacheDict(max_size=max_size, expiration=expiration)
+
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            key = repr(*args, **kwargs)
+            try:
+                result = CACHE[key]
+            except KeyError as e:
+                result = func(*args, **kwargs)
+
+                CACHE[key] = result
+            return result
+        return inner
+    return wrapper
+
