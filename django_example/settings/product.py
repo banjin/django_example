d2b83f7f3e4a049da045933ca0985e6024163439
songhm <songhm@qssec.com>
2019-04-30 14:22:56 +0800

    ”增加邮件配置“

--- django_example/settings/product.py
+++ django_example/settings/product.py
@@ -34,3 +34,76 @@ CACHES = {
     }
 }
 
+# 关闭浏览器时候设置session为过期 , 需要定时执行 python manage.py clearsessions,手动删除过期数据
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+
+# 配置邮件服务器
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 25
+EMAIL_HOST_PASSWORD = 'password' # 客户端授权码
+EMAIL_HOST_USER = '' # 邮箱用户名
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+            'format': '%(levelname)s [%(asctime)s] %(funcName)s %(message)s'
+            },
+        },
+        'filters': {
+		    'require_debug_false': {
+			    '()': 'django.utils.log.RequireDebugFalse',
+		    },
+		    'require_debug_true': {
+			    '()': 'django.utils.log.RequireDebugTrue',
+		    },
+	    },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'console_debug_false': {
+			    'level': 'ERROR',
+			    'filters': ['require_debug_false'],
+			    'class': 'logging.StreamHandler',
+		    },
+            'django.server': {
+			    'level': 'INFO',
+			    'class': 'logging.StreamHandler',
+			    'formatter': 'simple',
+		    },
+            # 'file': {
+            #     'class': 'logging.handlers.RotatingFileHandler',
+            #     'formatter': 'simple',
+            #     'filename': '/var/www/logs/ibiddjango.log',
+            #     'maxBytes': 1024000,
+            #     'backupCount': 3,
+            # },
+            'mail_admins': {
+                'level': 'ERROR',
+                'filters': ['require_debug_false'],
+                'class': 'django.utils.log.AdminEmailHandler'
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console','console_debug_false','mail_admins'],
+                'propagate': True,
+                'level': 'DEBUG',
+            },
+            'django.server': {
+			    'handlers': ['django.server'],
+			    'level': 'INFO',
+			    'propagate': False,
+		    }
+        }
+}
